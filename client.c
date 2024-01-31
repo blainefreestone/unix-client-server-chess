2eb93b2e10d016633cf742faeec631df03e4005b
blainefreestone <blainefreestone01@gmail.com>
2024-01-30 16:46:19 -0800

    server socket file descriptor received for client

--- client.c
+++ client.c
@@ -11,21 +11,21 @@ int main(void) {
 
   const char *PORT = "21202";                     // server port number
   const char *ADDRESS = "127.0.0.1";
-  struct addrinfo client_information;
+  struct addrinfo server_information;
 
   // set configuration for getaddrinfo():
-  client_information.ai_family = AF_UNSPEC;       // IPv4 or IPv6
-  client_information.ai_socktype = SOCK_STREAM;   // TCP Stream Socket
+  server_information.ai_family = AF_UNSPEC;       // IPv4 or IPv6
+  server_information.ai_socktype = SOCK_STREAM;   // TCP Stream Socket
 
-  struct addrinfo *client_address_info;
+  struct addrinfo *server_adress_info;
 
   printf("Getting host address information... ");
 
   int result = getaddrinfo(
     ADDRESS,
     PORT,
-    &client_information,
-    &client_address_info
+    &server_information,
+    &server_adress_info
   );
 
   // success and error messages
@@ -35,6 +35,20 @@ int main(void) {
   else {
     printf("%s\n", gai_strerror(result));
     exit(EXIT_FAILURE);
+  }
+
+  printf("Getting socket file descriptor... ");
+
+  int server_socket = socket(server_adress_info -> ai_family, server_adress_info -> ai_socktype, server_adress_info -> ai_protocol);   // get server socket descriptor
+
+  // success and error messages
+  if (server_socket == -1) {
+    printf("Failed\n");
+    printf("Error Number: %i\n", errno);
+    exit(EXIT_FAILURE);
+  }
+  else { 
+    printf("Success\n");
   }
 
   // TODO: connect to server
