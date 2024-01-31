c9d0651cda34588786e80807984f31070c9fc96e
blainefreestone <blainefreestone01@gmail.com>
2024-01-31 08:25:13 -0800

    client can send and receive strings to and from server

--- client.c
+++ client.c
@@ -10,12 +10,13 @@ int main(void) {
   // get socket file descriptor
 
   const char *PORT = "21202";                     // server port number
-  const char *ADDRESS = "127.0.0.1";
+  const char *ADDRESS = "10.0.2.15";
   struct addrinfo server_information;
 
   // set configuration for getaddrinfo():
   server_information.ai_family = AF_UNSPEC;       // IPv4 or IPv6
   server_information.ai_socktype = SOCK_STREAM;   // TCP Stream Socket
+  server_information.ai_flags = AI_PASSIVE;       // automatically fill in host IP
 
   struct addrinfo *server_adress_info;
 
@@ -51,9 +52,38 @@ int main(void) {
     printf("Success\n");
   }
 
-  // TODO: connect to server
+  // connect to server
+  
+  int connecting = connect(server_socket, server_adress_info -> ai_addr, server_adress_info -> ai_addrlen);   // establish connection to server socket
 
-  // TODO: communicate with server
+  // success and error messages
+  if (connecting == -1) {
+    printf("Failed\n");
+    printf("Error Number: %i\n", errno);
+    exit(EXIT_FAILURE);
+  }
+  else { 
+    printf("Success\n");
+  }
+
+  // communicate with server
+
+  // char send_message[100] = "Hello, server!\n";
+  int sending  = send(server_socket, "Hello, server!\n", 100, 0);
+
+  // success and error messages
+  if (sending == -1) {
+    printf("Failed to send message to client\n");
+    printf("Error Number: %i\n", errno);
+  }
+  else {
+    printf("Successfully sent %i bytes\n", sending);
+  }
+
+  char receive_message[100];
+  int receiving = recv(server_socket, receive_message, 100, 0);
+
+  printf("%s", receive_message);
 
   // TODO: close connection
 }
