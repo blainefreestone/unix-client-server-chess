c761d9db373c9aa09db83fd169a63827bb415556
blainefreestone <blainefreestone01@gmail.com>
2024-01-31 20:02:47 -0800

    can receive data from multiple clients at the same time

--- server.c
+++ server.c
@@ -18,31 +18,32 @@ int main(void) {
   // set configuration for getaddrinfo():
   server_information.ai_family = AF_UNSPEC;       // IPv4 or IPv6
   server_information.ai_socktype = SOCK_STREAM;   // TCP Stream Socket
-  server_information.ai_flags = AI_PASSIVE;       // automatically fill in host IP
+  server_information.ai_flags = AI_PASSIVE;       // automatically fill in host IP if address NULL
 
   struct addrinfo *server_adress_info;
 
   printf("Getting host address information... ");
 
   int result = getaddrinfo(
-    "10.0.2.15",                     // automatically use host ip
+    "10.0.2.15",              // host ip
     PORT,                     // use constant port number defined at top of file "21202"
     &server_information,      // points to struct with server information
     &server_adress_info       // points to result struct to hold address information
   );
 
   // success and error messages
-  if (result == 0) {                        // if addrinfo successfuly generated...
+  if (result == 0) {
     printf("Success\n");
   }
-  else {                                    // otherwise use gai_strerror to interpret error code and display
+  else {
     printf("%s\n", gai_strerror(result));
     exit(EXIT_FAILURE);
   }
 
   printf("Getting socket file descriptor... ");
 
-  int server_socket = socket(server_adress_info -> ai_family, server_adress_info -> ai_socktype, server_adress_info -> ai_protocol);   // get server socket descriptor
+  // get server socket descriptor
+  int server_socket = socket(server_adress_info -> ai_family, server_adress_info -> ai_socktype, server_adress_info -> ai_protocol);
 
   // success and error messages
   if (server_socket == -1) {
@@ -53,6 +54,7 @@ int main(void) {
   else { 
     printf("Success\n");
   }
+
 
   // Bind socket to port
 
@@ -70,6 +72,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
+
   // Listen on socket
 
   printf("Preparing to listen for connections on port %s...", PORT);
@@ -85,9 +88,11 @@ int main(void) {
     printf("Error Number: %i\n", errno);
   }
 
-  // Accept client socket
+
+  // Accept client sockets
   printf("Waiting for clients...\n");
 
+  int max_socket_fd = 0; // keeps track of highest socket file description for later select function
   int client_count = 0; // keeps track of number of clients connected
   int client_sockets[NUM_CLIENTS]; // array of client file descriptrs
   struct sockaddr_storage client_address;
@@ -104,11 +109,16 @@ int main(void) {
     }
     else {
       printf("Client %d succesfully connected\n", client_count + 1);
+
+      if (client_sockets[client_count] > max_socket_fd) {
+        max_socket_fd = client_sockets[client_count] + 1; // update max socket fd for later select function if it is higher than the current
+      }
+
       client_count++; // increase program count of clients
     }
 
     if (client_count >= NUM_CLIENTS) { // number of clients has reached maximum
-      printf("All clients connected\n");
+      printf("All %d client(s) connected\n", client_count);
       // declare client_one and client_two
       int client_one = client_sockets[0];
       int client_two = client_sockets[1];
@@ -116,20 +126,39 @@ int main(void) {
     }
   }
 
-  // Process request
+  // Interact with clients
 
-  printf("End");
-  return 0;
+  // Listen to clients
 
   while(1) {
-    char request[100];
-    int recieving = recv(client_sockets[1], request, 100, 0);
+    char message[100];
 
-    if (strcmp(request, "exit\n") == 0) {
-      break;
+    fd_set sockets;
+    FD_ZERO(&sockets);
+
+    for (int i = 0; i < 2; i++) {
+      FD_SET(client_sockets[i], &sockets);
     }
 
-    printf("%s", request);
+    int select_result = select(max_socket_fd, &sockets, NULL, NULL, NULL);
+
+    // error and failure messages
+    if (select_result == 0) {
+      printf("Did not receive any data\n");
+      break;
+    }
+    else if (select_result == -1) {
+      printf("Error\n");
+      printf("Error number: %d\n", errno);
+    }
+    // if successful, loop through socket file descriptors to find and store message
+    else {
+      for (int i = 0; i < 2; i++) {
+        if FD_ISSET(client_sockets[i], &sockets); {
+          recv(client_sockets[i], message, 100, 0);
+        }
+      }
+    }
   }
 
   // Reply to Client
