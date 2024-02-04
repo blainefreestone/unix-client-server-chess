0fd2b9660a80cbdf3855ae69697da7e7ad41ca28
blainefreestone <blainefreestone01@gmail.com>
2024-02-03 22:13:40 -0800

    basic logic of server communication added based on connection detials

--- client.c
+++ client.c
@@ -76,36 +76,65 @@ void get_connection_details(int server_socket, char *details) {
   printf("%s", details);
 }
 
+void receive_message(int server_socket, char *message) {
+  printf("Waiting for message from server\n");
+  int receiving = recv(server_socket, message, 100, 0);   // wait and receive message from client socket and save to message string
+  printf("Received message:\n%s", message);
+}
+
+void send_message(int server_socket, const char *message) {
+  send(server_socket, message, 100, 0);
+}
+
+int communicate(int server_socket) {
+  char send_message[100];
+  char message[100];
+  fgets(send_message, sizeof(send_message), stdin);
+
+  int sending  = send(server_socket, send_message, sizeof(send_message), 0);
+
+  // success and error messages
+  if (sending == -1) {
+    printf("Failed to send message to client\n");
+    printf("Error Number: %i\n", errno);
+  }
+  else {
+    printf("Successfully sent %i bytes\n", sending);
+  }
+
+  // exit condition
+  if (strcmp(send_message, "exit\n") == 0) {
+    return 0;
+  }
+
+  receive_message(server_socket, message);
+  printf("%s", message);
+
+  sleep(3);
+
+  receive_message(server_socket, message);
+  printf("%s", message);
+
+  return 1;
+}
+
 int main(void) {
   int server_socket;
   char connection_details[100];
+  char message[100];
 
   server_socket = get_and_connect_to_server_socket(IP, PORT);
   get_connection_details(server_socket, connection_details);
 
+  // if player_two, must listen for server before sending data to server
+  if (strcmp("player_two\n", connection_details) == 0) {
+    receive_message(server_socket, message);
+  }
 
-  // communicate with server
   while (1) {
-    char receive_message[100];
-    int receiving = recv(server_socket, receive_message, 100, 0);
+    status = communicate(server_socket);
 
-    printf("%s", receive_message);
-
-    char send_message[100];
-    fgets(send_message, sizeof(send_message), stdin);
-
-    int sending  = send(server_socket, send_message, sizeof(send_message), 0);
-
-    // success and error messages
-    if (sending == -1) {
-      printf("Failed to send message to client\n");
-      printf("Error Number: %i\n", errno);
-    }
-    else {
-      printf("Successfully sent %i bytes\n", sending);
-    }
-
-    if (strcmp(send_message, "exit\n") == 0) {
+    if (status == 0) {
       break;
     }
   }
