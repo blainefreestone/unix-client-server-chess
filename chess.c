46e94389bd0ef31ea3514d953be3e192bec3e407
blainefreestone <blainefreestone01@gmail.com>
2024-02-06 10:09:43 -0800

    make_move function started by decoding command string

--- chess.c
+++ chess.c
@@ -20,8 +20,9 @@
 
 #define BOARD_STRING_LENGTH 1000
 
-void get_start_board(char board[8][8]) {
+void set_board_start(char board[8][8]) {
   char start_board[8][8] = {
+  // board is 2d array of characters representing each type of piece.
     {B_ROOKE, B_KNIGHT, B_BISHOP, B_QUEEN,  B_KING, B_BISHOP, B_KNIGHT, B_ROOKE},
     {B_PAWN,  B_PAWN,   B_PAWN,   B_PAWN,   B_PAWN, B_PAWN,   B_PAWN,   B_PAWN},
     {EMPTY,   EMPTY,    EMPTY,    EMPTY,    EMPTY,  EMPTY,    EMPTY,    EMPTY},
@@ -38,6 +39,41 @@ void get_start_board(char board[8][8]) {
       board[i][j] = start_board[i][j];
     }
   }
+}
+
+int make_move(char board[8][8], char *command) {
+  // decode command string
+  int is_capture = 0;
+  int destination_char_index = 2;
+  int destination_int_index = 3;
+
+  // source location
+  int source_char = command[0] - 'a' + 1; // convert char a-h to int 1-8
+  int source_int = command[1] - '0';
+  
+  // invalid source location condition
+  if (source_char > 8 || source_int > 8 || source_char < 1 || source_int < 1) {
+    return -1;
+  }
+
+  // if command is --x-- it is a capture
+  if (command[2] == 'x') {
+    is_capture = 1;
+    // update indexes for destination chars
+    destination_char_index = 3;
+    destination_int_index = 4;
+  }
+
+  // destination location
+  int destination_char = command[destination_char_index] - 'a' + 1; // convert char a-h to int 1-8
+  int destination_int = command[destination_int_index] - '0';
+
+  // invalid destination location condition
+  if (destination_char > 8 || destination_int > 8 || destination_char < 1 || destination_int < 1) {
+    return -1;
+  }
+
+  printf("%d %d %d %d", source_char, source_int, destination_char, destination_int);
 }
 
 char* get_board_text(char board[8][8]) {
